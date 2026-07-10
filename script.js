0aa567f32a803151042b4f183cf003fe1a2b944a
ruttunanmol-hash <ruttunanmol@gmail.com>
2026-07-10 14:50:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 document.getElementById("giftButton").addEventListener("click",()=>{
 
-alert("Your first gift is coming soon... 💜");
+    document.querySelector(".container").innerHTML = `
+
+        <h1>For My Mi Amor 💜</h1>
+
+        <p class="heart">💜</p>
+
+        <p class="text">
+            Happy Birthday, Jose.
+        </p>
+
+        <p class="text">
+            I made this little universe just for you.
+        </p>
+
+        <button id="memoryButton">
+            ✨ Open Our Story
+        </button>
+
+    `;
 
 });
